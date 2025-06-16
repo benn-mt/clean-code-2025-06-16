b6b78a0b5529994c1bbeb4eef0f63e7143cba3af
Ben Noble <ben.noble@madetech.com>
2025-06-16 15:58:12 +0100

    Minimum for equals, and, not and or

--- src/chance.py
+++ src/chance.py
@@ -1,7 +1,22 @@
 class Chance:
+    __CERTAIN_LIKELIHOOD = 1.0
+    __THRESHOLD = 0.000001
+
     def __init__(self, likelihood):
         self._likelihood = likelihood
 
     # override == operator
     def __eq__(self, other):
-        return self._likelihood == other._likelihood
+        return abs(self._likelihood - other._likelihood) < self.__THRESHOLD
+
+    def not_(self):
+        return Chance(self.__CERTAIN_LIKELIHOOD - self._likelihood)
+    
+    def and_(self, other):
+        return Chance(self._likelihood * other._likelihood)
+    
+    def or_(self, other):
+        return Chance(self._likelihood + other._likelihood - (self.and_(other)._likelihood))
+    
+    def __repr__(self):
+     return "Chance(%.10f)" % self._likelihood
--- test/test_chance.py
+++ test/test_chance.py
@@ -1,10 +1,32 @@
 import unittest
 from src.chance import Chance
 
+CERTAIN = Chance(1)
+IMPOSSIBLE = Chance(0)
+FIFTY_FIFTY = Chance(0.5)
+
 class TestChance(unittest.TestCase):
     def test_chances_are_equal_if_created_with_equal_likelihoods(self):
         self.assertEqual(Chance(1),Chance(1))
         self.assertNotEqual(Chance(1),Chance(0))
+
+    def test_not_is_inverse_of_probablility(self):
+        self.assertEqual(FIFTY_FIFTY, FIFTY_FIFTY.not_())
+        self.assertEqual(CERTAIN, IMPOSSIBLE.not_())
+        self.assertEqual(IMPOSSIBLE, IMPOSSIBLE.not_().not_())
+        self.assertEqual(Chance(0.4).not_(), Chance(0.6))
+        self.assertEqual(Chance(1.0 / 3.0).not_(), Chance(2.0 / 3.0))
+
+    def test_can_be_combined_with_and(self):
+        self.assertEqual(CERTAIN.and_(CERTAIN), CERTAIN)
+        self.assertEqual(CERTAIN.and_(IMPOSSIBLE), IMPOSSIBLE)
+        self.assertEqual(CERTAIN.and_(FIFTY_FIFTY), FIFTY_FIFTY)
+        self.assertEqual(FIFTY_FIFTY.and_(FIFTY_FIFTY), Chance(0.25))
+
+    def test_can_be_combined_with_or(self):
+        self.assertEqual(CERTAIN.or_(CERTAIN), CERTAIN)
+        self.assertEqual(FIFTY_FIFTY.or_(FIFTY_FIFTY), Chance(0.75))
+
 
 if __name__ == '__main__':
     unittest.main()
